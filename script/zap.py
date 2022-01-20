7be76debe04cc20ea759c902f45fa534cb55e914
san3ncrypt3d <san3ncrypt3d@san3ncrypt3d.local>
2022-01-20 12:39:24 -0500

    scriot

---
+++ script/zap.py
@@ -0,0 +1,64 @@
+from zapv2 import ZAPv2 as ZAP
+import time
+import requests
+import datetime
+from os import getcwd
+
+#target url for scan
+
+target = 'https://gse.manulife.com'
+
+#apikey = '8ij7v7nl0t6d777okrh4kf3icb'
+
+
+
+zap = ZAP(proxies={'http':'http://10.234.45.19:8081','https':'http://10.234.45.19:8081'})
+
+zap.urlopen(target)
+
+
+
+#Passive scan
+
+while (int(zap.pscan.records_to_scan) > 0):
+
+    print ('Passive Scan Records %: ' + zap.pscan.records_to_scan)
+
+    time.sleep(5)
+
+
+
+print('Passive Scan Completed..!')
+
+#Active Scan
+
+active_scan_id = zap.ascan.scan(url=target)
+
+time.sleep(5)
+
+while int(zap.ascan.status(active_scan_id)) < 100:
+
+    print ('Active Scan  %: ' + zap.ascan.status())
+
+    time.sleep(5)
+
+print ('Active Scan completed..!')
+
+
+
+# HTML Report
+with open ('report.html', 'w') as f:f.write(zap.core.htmlreport())
+# XML Report
+#with open ('report.xml', 'w') as f:f.write(zap.core.xmlreport(apikey = 'apikey'))
+
+now = datetime.datetime.now().strftime("%m/%d/%Y")
+alert_severity = 't;t;t;t'  # High;Medium;Low;Info
+# CWEID;#WASCID;Description;Other Info;Solution;Reference;Request Header;Response Header;Request Body;Response Body
+alert_details = 't;t;t;t;t;t;f;f;f;f'
+source_info = 'Vulnerability Report ;{};{};v1;v1; Scan Report'.format(
+    now, now)
+path = getcwd() + "/zap-report.json"
+zap.exportreport.generate(path, "json", sourcedetails=source_info,
+                          alertseverity=alert_severity, alertdetails=alert_details, scanid=active_scan_id)
+
+zap.core.shutdown()
